b4404dd86c58c355712f416a5fc3a415afd5ac70
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-11 22:30:34 +0300

    end task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter number 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter number 2");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Max = " + a);
+    Console.WriteLine("Min = " + b);
+}
+else
+{
+    Console.WriteLine("Max = " + b);
+    Console.WriteLine("Min = " + a);
+}
+Console.WriteLine("end");
