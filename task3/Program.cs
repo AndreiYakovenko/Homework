7015175baf02d703e45b91dfd1f029a305bc1849
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-12 07:45:38 +0300

    end task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please enter number");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine("This number is even");
+}
+else
+{
+    Console.WriteLine("This number is not even");
+}
