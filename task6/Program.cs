66dc7ac5fcfc5b8022fab06dbd6e67ab315a084d
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-13 09:38:14 +0300

    end task6

---
+++ task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Please Enter any number:");
+int number = Convert.ToInt32(Console.ReadLine());
+var str = number.ToString();
+if (str.Length>2) Console.WriteLine("Third number - " + str[2].ToString());
+else Console.WriteLine("No third digit.");
