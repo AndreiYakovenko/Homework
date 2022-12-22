bdbfd9f44231635dc899e83e23a519da5cb280a6
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-22 08:47:00 +0300

    end task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Please enter a five-digit number");
+﻿// Напишите программу,
+// которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+Console.WriteLine("Please enter a five-digit number");
 int n = Convert.ToInt32(Console.ReadLine());
 int d1 = n/10000;
 int d2 = n/1000%10;
@@ -6,9 +10,3 @@ int d4 = n/10%10;
 int d5 = n%10;
 if(d1==d5 && d2==d4) Console.WriteLine("This number is a palindrome!");
 else Console.WriteLine("This number is not a palindrome");
-
-// Console.WriteLine(d1);
-// Console.WriteLine(d2);
-// Console.WriteLine(d3);
-// Console.WriteLine(d4);
-// Console.WriteLine(d5);
