9df2b669abe598b5e6ba7e6f370f19c940520bf8
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-22 14:07:18 +0300

    end task29

---
+++ task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Enter the desired array size (up to 10 elements):");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 101);
+    Console.Write(array[i] + " ");
+}
