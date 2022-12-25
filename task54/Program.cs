6237a931e3e352bb13b05f55cb85d124fceea0c9
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-25 17:57:42 +0300

    end task54

---
+++ task54/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+
+Console.WriteLine("Please enter the number of lines:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the number of columns:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+
+void FillArray(int m, int n)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(m, n);
+PrintArray(array);
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - j; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                (array[i, k], array[i, k + 1]) = (array[i, k + 1], array[i, k]);
+            }
+        }
+    }
+}
+
+PrintArray(array);
+Console.WriteLine();
