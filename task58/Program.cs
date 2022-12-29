78c9bea4b6ba92dabe2d582eae2ebd231adc7633
AndreiYakovenko <andrei.yakovenko@gmail.com>
2022-12-30 00:13:29 +0300

    start task58

---
+++ task58/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте две матрицы. Напишите программу,
+// которая будет находить произведение двух матриц.
+
+Console.WriteLine("Please enter the number of lines:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the number of columns:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+
+void FillArray(int m, int n)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(m, n);
+PrintArray(array);
+Console.WriteLine();
