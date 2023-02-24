10e07b29f358a5203b39d13ce304f9b6285928e0
AndreiYakovenko <andrei.yakovenko@gmail.com>
2023-02-24 17:51:57 +0300

    create VerificationWork

---
+++ VerificationWork/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] originalArray = {"hello", "2", "world", ":)"};
+List<string> shorterStrings = new List<string>();
+
+foreach (string str in originalArray)
+{
+    if (str.Length <= 3)
+    {
+        shorterStrings.Add(str);
+    }
+}
+
+string[] newArray = shorterStrings.ToArray();
